d55baa8678f39a45be25adb16da3ff142c084a29
Phuc <hohap3@gmail.com>
2022-05-31 18:35:43 +0700

    Update file

--- JS/api/postAPI.js
+++ JS/api/postAPI.js
@@ -1,4 +1,4 @@
-import { axiosClient } from './axiosClient.js'
+import  axiosClient from './axiosClient.js'
 
 const postAPI = {
   
--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
-console.log("Hello");
+import postAPI from './api/postAPI.js'
 
-console.log('Hi')
+async function main() {
+
+  try {
+    
+    const params = {
+      _page:1,
+      _limit:5
+    }
+
+    const response = await postAPI.getAllPost(params);
+    console.log(response); 
+
+  }catch(error) {
+    console.error(error)
+  }
+  
+}
+
+main();
